4904c2c50143f9e620237cde21e3ae6015ee1b9f
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-03-11 12:49:51 -0600

    fixing a couple of typos of the Javascript file

--- js/application.js
+++ js/application.js
@@ -1,23 +1,19 @@
-
-
 App.onLaunch = function(options) {
-
-	var alert = createAlert("Hello World", "");
-	navigationDocument.presentModal(alert);
+  // 1
+  var alert = createAlert("Welcome to my first Apple TV app", ""); //leaving 2nd parameter with an empty string
+  navigationDocument.presentModal(alert);
 }
-
+ 
+// 2
 var createAlert = function(title, description) {
-	var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
-		<document>
-			<alertTemplate>
-				<title>${title}</title>
-				<description>${description}</description>
-			</alertTemplate>
-		</document>
-
-	var parser = new DOMParser();
-	var alertDoc = parser.parseFromString(alertString, "application/xml");
-
-	return alertDoc
+  var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+      <alertTemplate>
+        <title>${title}</title>
+        <description>${description}</description>
+      </alertTemplate>
+    </document>`
+    var parser = new DOMParser();
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+    return alertDoc
 }
-
