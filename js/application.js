983210c95fbe7fbc6da956d4917d83e737938232
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-03-11 12:37:00 -0600

    Creating an alert in Javascript;

---
+++ js/application.js
@@ -0,0 +1,23 @@
+
+
+App.onLaunch = function(options) {
+
+	var alert = createAlert("Hello World", "");
+	navigationDocument.presentModal(alert);
+}
+
+var createAlert = function(title, description) {
+	var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+		<document>
+			<alertTemplate>
+				<title>${title}</title>
+				<description>${description}</description>
+			</alertTemplate>
+		</document>
+
+	var parser = new DOMParser();
+	var alertDoc = parser.parseFromString(alertString, "application/xml");
+
+	return alertDoc
+}
+
